f72a25d8a4e998baa37e39a3c1af999d2b2cbe0c
Cledylson <124412032+Cledylson@users.noreply.github.com>
2023-02-04 10:36:28 +0900

    Create numeros.py

---
+++ numeros.py
@@ -0,0 +1,11 @@
+print(40*"-")
+print("Vamos colocar 5 numeros e descobrir qual deles é o menor e o maior ?")
+x=int(input("Digite o primeiro número: "))
+y=int(input("Digite o segundo número: "))
+c=int(input("Digite o terceiro número: "))
+z=int(input("Digite o quarto número: "))
+f=int(input("Digite o quinto número: "))
+
+a = [x,y,c,z,f]
+print("O valor do maior numero é ",max(a))
+print("O valor do maior numero é ",min(a))
